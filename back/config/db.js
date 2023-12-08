b20d19785fd3a4aa1d64b83dec1ce2d88e44e548
sidx <sidsigma3@gmail.com>
2023-12-08 13:02:30 +0530

    changes

--- back/config/db.js
+++ back/config/db.js
@@ -1,9 +1,10 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "",
-database:"login"
-})
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DBNAME || "login",
+});
 
 module.exports = db;
